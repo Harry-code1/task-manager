d79a66d570322a239bf1fa79c7cb6e1a7d5ddd47
Harry-Code1 <harrywearn@icloud.com>
2025-12-25 22:07:07 +0000

    Add ability to mark tasks as complete

--- task_manager.py
+++ task_manager.py
@@ -28,6 +28,27 @@ def add_task(tasks):
     save_tasks(tasks)
     print("Task added.")
 
+def complete_task(tasks):
+    show_tasks(tasks)
+
+    if not tasks:
+        return
+
+    choice = input("Enter task number to mark as complete: ")
+
+    if not choice.isdigit():
+        print("Please enter a valid number.")
+        return
+    index = int(choice) - 1
+
+    if index < 0 or index >= len(tasks):
+        print("Task number out of range.")
+        return
+
+    tasks[index]["done"] = True
+    save_tasks(tasks)
+    print("Task marked as complete.")
+
 def main():
     tasks = load_tasks()
 
@@ -35,7 +56,8 @@ def main():
         print("\nTask Manager")
         print("1. Show tasks")
         print("2. Add task")
-        print("3. Exit")
+        print("3. Complete task")
+        print("4. Exit")
 
         choice = input("Choose and option: ")
 
@@ -44,6 +66,8 @@ def main():
         elif choice == "2":
             add_task(tasks)
         elif choice == "3":
+            complete_task(tasks)
+        elif choice == "4":
             print("Goodbye!")
             break
         else:
